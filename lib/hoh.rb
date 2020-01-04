06ff425738b47d10b515dd75960ccf04b165f083
Halton Bagley <haltonba94@gmail.com>
2020-01-04 17:59:35 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,13 +31,12 @@
 # }
 
 def naming_system
-  PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
- PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
- PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4
-
-
-
- PORTION_1
+  taxonomic_rank = {
+    PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
+    PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
+    PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4
+  }
+  taxonomic_rank
   # Remember:
   #  Kingdom
   #  Phylum

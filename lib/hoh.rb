5d4a720a50fecb9778cf58d1e6b75f5920bd07ef
Halton Bagley <haltonba94@gmail.com>
2020-01-04 18:10:32 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,7 +4,10 @@
 #
 # The tests will guide your construction.
 #
- PORTION_1 = {
+ 
+
+def naming_system
+  PORTION_1 = {
    label: "Kingdom",
    sub_category: {
      label: "Phylum",
@@ -13,18 +16,15 @@
      }
    }
  }
- 
  PORTION_2 = {
    label: "Order"
  }
- 
  PORTION_3 = {
    label: "Family",
    sub_category: {
      label: "Genus",
    }
  }
- 
  PORTION_4 = {
    label: "Species",
    sub_category: nil
@@ -34,9 +34,5 @@
     PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
     PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4
 
-def naming_system
-  taxonomic_rank = {
-    PORTION_1
-  }
-  taxonomic_rank
+PORTION_1
 end

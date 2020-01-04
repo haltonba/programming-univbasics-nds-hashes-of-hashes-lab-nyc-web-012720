19bbf074d54b365e25b004fcc6f6326c5e9f6130
Halton Bagley <haltonba94@gmail.com>
2020-01-04 18:05:45 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -29,12 +29,14 @@
    label: "Species",
    sub_category: nil
  }
+ 
+ PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
+    PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
+    PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4
 
 def naming_system
   taxonomic_rank = {
-    PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
-    PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
-    PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4
+    PORTION_1
   }
   taxonomic_rank
 end

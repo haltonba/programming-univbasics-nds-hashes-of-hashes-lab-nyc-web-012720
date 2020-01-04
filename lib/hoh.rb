821d2b2d4ed428e6d1683df6484f719fa48fb37f
Halton Bagley <haltonba94@gmail.com>
2020-01-04 18:01:30 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -37,15 +37,4 @@ def naming_system
     PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4
   }
   taxonomic_rank
-  # Remember:
-  #  Kingdom
-  #  Phylum
-  #  Class
-  #  Order
-  #  Family
-  #  Genus
-  #  Species
-  # So, if we have the "Kingdom" node we should be able to "tunnel" into the
-  # HoH all the way to Species!
-
 end
